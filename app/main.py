d5d9e9517b6992390e3eacecc86d49befd144a7c
Dr-Kate <katia.01.01.2007d@gmail.com>
2026-04-17 18:25:32 +0300

    format the task

--- app/main.py
+++ app/main.py
@@ -1,21 +1,23 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line"],
-        "column": error["column"],
-        "message": error["message"],
-        "name": error["name"],
-        "source": error["source"],
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "file_path": file_path,
+        "path": file_path,
         "errors": [format_linter_error(e) for e in errors],
         "status": "passed" if len(errors) == 0 else "failed",
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errors)
-        for path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
